88de4d8081f8ffabbdb8a5e6ee062d2363fead97
Felipe Wandork <wandork@gmail.com>
2022-06-19 19:17:11 -0300

    Autenticação via Github finalizada

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
-export function SignInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
-        <button type="button"
-            className={styles.signInButton}>
-            <FaGithub color="#84d361" />Felipe Wandork
-            <FiX color="#737380" className={styles.closeIcon} />
-        </button>
-    ) : (
-        <button type="button"
-            className={styles.signInButton}>
-            <FaGithub color="#eba417" />Sign in with Github
-        </button >
-    );
+export default function SignInButton() {
+    const { data: session } = useSession();
+    if (session) {
+        return (
+            <>
+                <button type="button" className={styles.signInButton} 
+                    onClick={() => signOut()}>
+                    <FaGithub color="#84d361" />
+                    {session.user.name}
+                    <FiX color="#737380" className={styles.closeIcon} />
+                </button>
+            </>
+        )
+    }
+    return (
+        <>
+            <button type="button" className={styles.signInButton} 
+                onClick={() => signIn()}>
+                <FaGithub color="#eba417" />Sign in with Github
+            </button>
+        </>
+    )
 }
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -3,11 +3,12 @@ import styles from './styles.module.scss';
 interface SubscribeButtonProps {
     priceId: string;
 }
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-
+export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
     return (
+        <>
         <button type="button" className={styles.subscribeButton}>
             Subscribe Now
         </button>
+        </>
     )
 }
