e775934ea962652ba72b7029c73724fa0282a4f4
Felipe Wandork <wandork@gmail.com>
2022-06-24 10:54:10 -0300

    Implantação do FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
     providers: [
@@ -8,4 +11,24 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
     ],
-});
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { bio } = user;
+
+            await fauna.query(
+                q.Create(
+                    q.Collection('users'),
+                    { data: { bio } }
+                )
+            )
+                .then((ret) => console.log(ret))
+                .catch((err) => console.error(
+                    'Error: [%s] %s: %s',
+                    err.name,
+                    err.message,
+                    err.errors()[0].description,
+                ))
+            return true
+        },
+    }
+})
