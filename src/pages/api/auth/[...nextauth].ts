3a4855000ab668b1027224390513af98be65c1f8
Felipe Wandork <wandork@gmail.com>
2022-06-29 23:34:39 -0300

    Para correção pelo fórum

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,26 +9,43 @@ export default NextAuth({
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    scope: 'read:user',
+                },
+            },
         }),
     ],
     callbacks: {
-        async signIn(user, account, profile) {
-            const { bio } = user;
-
-            await fauna.query(
-                q.Create(
-                    q.Collection('users'),
-                    { data: { bio } }
+        async signIn({ user, account, profile }) {
+            const { email } = user;
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
                 )
-            )
-                .then((ret) => console.log(ret))
-                .catch((err) => console.error(
-                    'Error: [%s] %s: %s',
-                    err.name,
-                    err.message,
-                    err.errors()[0].description,
-                ))
-            return true
+                return true 
+            } catch {
+                return false
+            }
         },
     }
 })
